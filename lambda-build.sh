d465c23ff019bbdbe896968b036a9595e74d60c8
Phil Bambridge <phil.bambridge@ons.gov.uk>
2021-03-03 09:21:58 +0000

    Fix build with new lambdas path

--- lambda-build.sh
+++ lambda-build.sh
@@ -7,9 +7,9 @@
 ZIPFILE="key-rotator-appsync.zip"
 
 apk add py3-pip zip
-cd lambda
+cd lambdas
 pip install --ignore-installed --prefix=./ -r requirements.txt
 cd lib/python3.8/site-packages
 zip -qr $OLDPWD/$ZIPFILE ./*
 cd $OLDPWD
-zip -jg $ZIPFILE ../lambda/*
+zip -jg $ZIPFILE ../lambdas/*
